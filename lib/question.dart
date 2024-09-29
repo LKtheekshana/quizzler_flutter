09262bb099c9c7467fbfcf41566bb32bb0e8c9a4
Isuru Theekshana <Isuruace@gmail.com>
2024-09-29 12:03:52 +0530

    Initial commit

---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  String questionText;
+  bool questionAnswer;
+
+  // Constructor with required keyword for named parameters
+  Question({required this.questionText, required this.questionAnswer});
+}
+
+// Question newQuestion = Question(questionText: 'text', questionAnswer: true);
